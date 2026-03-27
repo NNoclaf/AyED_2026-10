7f74dcf732005157d5677d396de157c2745e14ee
NNoclaf <nfalcon876@gmail.com>
2026-03-27 16:07:36 -0300

    Add files via upload

---
+++ El usuario y el cine - Falcon Nahuel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_usuario_y_el_cine___Falcon_Nahuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String pelicula;
+
+            Console.WriteLine("Hola");
+            Console.Write("Como te llamas?: ");
+            nombre = Console.ReadLine();
+            Console.Write("Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+
+         
+            Console.WriteLine("Hola "  +  nombre);
+            Console.WriteLine("Tu pelicula preferida " + pelicula + " es una porqueria");
+        }
+    }
+}
