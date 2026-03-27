5bd84f37c202b2acaf82bfe548f490da0c162c45
NNoclaf <nfalcon876@gmail.com>
2026-03-27 16:44:16 -0300

    Add files via upload

---
+++ Calculo de edad - Falcon Nahuel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_edad___Falcon_Nahuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            Console.Write("En que año naciste?: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            int edad = 2026 - año;
+            Console.WriteLine("Si naciste en el año " + año + " tu edad al comenzar el año es de " + edad + " años");
+        }
+    }
+}
