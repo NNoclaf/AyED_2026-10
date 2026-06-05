c257db823e83e3678a74b1167b07e3df0378b214
NNoclaf <nfalcon876@gmail.com>
2026-06-05 18:08:42 -0300

    Add files via upload

---
+++ GUIA WHILE  DO - WHILE Nº2/ejercicio 2 guia 2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerc_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSecreto = 11;
+            int usuario = 0;
+            int intentos = 0;
+
+            while (usuario != numeroSecreto && intentos < 5)
+            {
+                Console.Write("Ingrese un numero: ");
+                usuario = int.Parse(Console.ReadLine());
+
+                intentos++;
+
+                if (usuario < numeroSecreto)
+                {
+                    Console.WriteLine("El numero es mayor");
+                }
+                else
+                {
+                    if (usuario > numeroSecreto)
+                    {
+                        Console.WriteLine("El numero es menor");
+                    }
+                }
+            }
+
+            if (Usuario == numeroSecreto)
+            {
+                Console.WriteLine("Ganaste");
+
+                Console.WriteLine("Intentos usados: " + intentos);
+            }
+            else
+            {
+                Console.WriteLine("Perdiste");
+
+                Console.WriteLine("Intentos usados: " + intentos);
+            }
+        }
+    }
+}
---
+++ GUIA WHILE  DO - WHILE Nº2/ejercicio 5 guia 2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldo = 10000;
+            int opci = 0;
+
+            while (opci != 4)
+            {
+                Console.WriteLine("1. Depositar dinero");
+                Console.WriteLine("2. Retirar dinero");
+                Console.WriteLine("3. Ver saldo actual");
+                Console.WriteLine("4. Salir");
+
+                Console.Write("Ingrese una de las opciones: ");
+                opci = int.Parse(Console.ReadLine());
+
+                switch (opci)
+                {
+                    case 1:
+
+                        Console.Write("Ingrese el dinero que va a depositar: ");
+                        int deposito = int.Parse(Console.ReadLine());
+
+                        saldo = saldo + deposito;
+
+                        Console.WriteLine("Saldo actual: $" + saldo);
+
+                        break;
+
+                    case 2:
+
+                        Console.Write("Ingrese el dinero que va a retirar: ");
+                        int retiro = int.Parse(Console.ReadLine());
+
+                        saldo = saldo - retiro;
+
+                        Console.WriteLine("Saldo actual: $" + saldo);
+
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Su saldo actual es: $" + saldo);
+
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Saliendo del cajero");
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Opción invalida");
+
+                        break;
+                }
+            }
+        }
+    }
+}
