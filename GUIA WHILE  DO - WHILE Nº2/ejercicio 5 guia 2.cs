434e120d4e37e20a0a1357ac74f251ce4d3482b8
NNoclaf <nfalcon876@gmail.com>
2026-06-05 18:52:39 -0300

    ejercicio 5 guia 2.cs

--- GUIA WHILE  DO - WHILE Nº2/ejercicio 5 guia 2.cs
+++ GUIA WHILE  DO - WHILE Nº2/ejercicio 5 guia 2.cs
@@ -1,71 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ejerc1
+namespace ejer5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int saldo = 10000;
-            int opci = 0;
+            int positivos = 0;
+            int negativos = 0;
+            int ceros = 0;
 
-            while (opci != 4)
+            Console.Write("Ingrese un numero o X/n para salir: ");
+            string dato = Console.ReadLine();
+
+            while (dato != "X" && dato != "n")
             {
-                Console.WriteLine("1. Depositar dinero");
-                Console.WriteLine("2. Retirar dinero");
-                Console.WriteLine("3. Ver saldo actual");
-                Console.WriteLine("4. Salir");
+                int numero = int.Parse(dato);
 
-                Console.Write("Ingrese una de las opciones: ");
-                opci = int.Parse(Console.ReadLine());
-
-                switch (opci)
+                if (numero > 0)
                 {
-                    case 1:
-
-                        Console.Write("Ingrese el dinero que va a depositar: ");
-                        int deposito = int.Parse(Console.ReadLine());
-
-                        saldo = saldo + deposito;
-
-                        Console.WriteLine("Saldo actual: $" + saldo);
-
-                        break;
-
-                    case 2:
-
-                        Console.Write("Ingrese el dinero que va a retirar: ");
-                        int retiro = int.Parse(Console.ReadLine());
-
-                        saldo = saldo - retiro;
-
-                        Console.WriteLine("Saldo actual: $" + saldo);
-
-                        break;
-
-                    case 3:
-
-                        Console.WriteLine("Su saldo actual es: $" + saldo);
-
-                        break;
-
-                    case 4:
-
-                        Console.WriteLine("Saliendo del cajero");
-
-                        break;
-
-                    default:
-
-                        Console.WriteLine("Opción invalida");
-
-                        break;
+                    positivos++;
                 }
+                else
+                {
+                    if (numero < 0)
+                    {
+                        negativos++;
+                    }
+                    else
+                    {
+                        ceros++;
+                    }
+                }
+
+                Console.Write("Ingrese un número o X/n para salir: ");
+                dato = Console.ReadLine();
             }
+
+            Console.WriteLine("Cantidad de positivos: " + positivos);
+            Console.WriteLine("Cantidad de negativos: " + negativos);
+            Console.WriteLine("Cantidad de ceros: " + ceros);
         }
     }
 }
