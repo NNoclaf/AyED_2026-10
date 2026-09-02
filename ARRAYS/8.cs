74ff993876e51f32990db6c177635ad49a5e74ac
NNoclaf <nfalcon876@gmail.com>
2026-09-02 20:31:12 -0300

    Update 8.cs

--- ARRAYS/8.cs
+++ ARRAYS/8.cs
@@ -4,31 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication5
+namespace ConsoleApplication9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int producto = 0;
-            int[] precio = new int[8];
+            int uno = 0;
+int dos = 0;
+int tres = 0;
+int cuatro = 0;
+int cinco = 0;
 
-            for (int i =0; i < precio.Length; i++)
-            {
-                Console.Write("escriba cuanto vale el producto: ");
-                precio[i] = int.Parse(Console.ReadLine());
-            }
-            Console.Write("ponga cuanto dinero hay: ");
-            int dinero = int.Parse(Console.ReadLine());
+int[] respuestas = new int[25];
 
-            for (int i =0; i < precio.Length; i++)
-            {
-                if (precio[i]<= dinero)
-                {
-                    producto++;
-                }
-            }
-            Console.WriteLine("se puede comprar " + producto + " productos");
+for (int i = 0; i < respuestas.Length; i++)
+{
+    Console.Write("calificque del 1 al 5: ");
+    respuestas[i] = int.Parse(Console.ReadLine());
+
+    if (respuestas[i] == 1)
+    {
+        uno++;
+    }
+    else if (respuestas[i] == 2)
+    {
+        dos++;
+    }
+    else if (respuestas[i] == 3)
+    {
+        tres++;
+    }
+    else if (respuestas[i] == 4)
+    {
+        cuatro++;
+    }
+    else if (respuestas[i] == 5)
+    {
+        cinco++;
+    }
+}
+
+Console.WriteLine("cantidad de 1: " + uno);
+Console.WriteLine("cantidad de 2: " + dos);
+Console.WriteLine("cantidad de 3: " + tres);
+Console.WriteLine("cantidad de 4: " + cuatro);
+Console.WriteLine("cantidad de 5: " + cinco);
         }
     }
 }
