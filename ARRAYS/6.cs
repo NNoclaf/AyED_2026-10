dc2d1efb10ffce6d83c44767357ce562bd95b92c
NNoclaf <nfalcon876@gmail.com>
2026-09-02 20:21:25 -0300

    Update 6.cs

--- ARRAYS/6.cs
+++ ARRAYS/6.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,29 +6,47 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication5
 {
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int producto = 0;
-            int[] precio = new int[8];
+       int total1 = 0;
+int total2 = 0;
 
-            for (int i =0; i < precio.Length; i++)
-            {
-                Console.Write("escriba cuanto vale el producto: ");
-                precio[i] = int.Parse(Console.ReadLine());
-            }
-            Console.Write("ponga cuanto dinero hay: ");
-            int dinero = int.Parse(Console.ReadLine());
+int[] sucursal1 = new int[5];
+int[] sucursal2 = new int[5];
 
-            for (int i =0; i < precio.Length; i++)
-            {
-                if (precio[i]<= dinero)
-                {
-                    producto++;
-                }
-            }
-            Console.WriteLine("se puede comprar " + producto + " productos");
-        }
+for (int i = 0; i < sucursal1.Length; i++)
+{
+Console.Write("ponga venta de sucursal 1: ");
+sucursal1[i] = int.Parse(Console.ReadLine());
+
+Console.Write("ponga venta de sucursal 2: ");
+sucursal2[i] = int.Parse(Console.ReadLine());
+
+total1 = total1 + sucursal1[i];
+total2 = total2 + sucursal2[i];
+}
+
+for (int i = 0; i < sucursal1.Length; i++)
+{
+if (sucursal1[i] > sucursal2[i])
+{
+    Console.WriteLine("el dia " + (i + 1) + " vendio mas la sucursal 1");
+}
+else (sucursal2[i] > sucursal1[i])
+{
+    Console.WriteLine("el dia " + (i + 1) + " vendio mas la sucursal 2");
+}
+
+if (total1 > total2)
+{
+Console.WriteLine("sucursal 1 recaudo mas en la semana");
+}
+else (total2 > total1)
+{
+Console.WriteLine("sucursal 2 recaudo mas en la semana");
+}
     }
+}
 }
