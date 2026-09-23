802e087a86839bd14fa2b3e16386f7892dc51397
NNoclaf <nfalcon876@gmail.com>
2026-09-22 21:07:18 -0300

    Update fun 2.cs

--- FUNCIONES 2/fun 2.cs
+++ FUNCIONES 2/fun 2.cs
@@ -4,67 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("ponga la cantidad de numeros: ");
-        int cantida = int.Parse(Console.ReadLine());
-        mostrar(cantida);
+       int numero;
 
-    }
-        static void mostrar(int cantida)
+        Console.Write("ponga un numero entero y qsea positivo: ");
+        numero = int.Parse(Console.ReadLine());
+
+        if (Perfecto(numero))
         {
-            int suma = 0;
-            int menor = 0;
-            int mayor = 0;
-            int par = 0;
-            int impar = 0;
+            Console.WriteLine("el numero es perfecto.");
+        }
+        else
+        {
+            Console.WriteLine("el numero no es perfecto.");
+        }
+    }
 
-            for (int i = 1; i < cantida; i++)
+    static bool Perfecto(int numero)
+    {
+        int suma = 0;
 
+        for (int i = 1; i < numero; i++)
+        {
+            if (numero % i == 0)
             {
-                Console.Write("ponga un numero: ");
-                int numero = int.Parse(Console.ReadLine());
-
-                if (i == 1)
-                {
-                    mayor = numero;
-                    menor = numero;
-                }
-                if (numero > mayor)
-                {
-                    mayor = numero;
-                }
-                if (numero < menor)
-                {
-                    menor = numero;
-                }
-             
-
-                if (numero % 2 == 0)
-                {
-                    par++;
-                }
-                else
-                {
-                    impar++;
-                }
+                suma = suma + i;
             }
-            int promedio = suma / cantida;
+        }
 
-            Console.WriteLine(" el numero mayor es: " + mayor);
-            Console.WriteLine(" el menor numero es : " + menor);
-            Console.WriteLine(" los numeros pares son: " + par);
-            Console.WriteLine(" los numros impar son: " + impar);
-            Console.WriteLine(" el promediode todo es: " + promedio);
-      
-
-
-}
+        return suma == numero;
     }
 }
-
-
