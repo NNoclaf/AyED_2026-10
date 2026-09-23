8ed08b886551c73c4e80d81f6c429a561a71dc55
NNoclaf <nfalcon876@gmail.com>
2026-09-22 22:39:51 -0300

    Update fun 4.cs

--- FUNCIONES 2/fun 4.cs
+++ FUNCIONES 2/fun 4.cs
@@ -60,65 +60,7 @@ class Program
             Console.WriteLine(" los numeros pares son: " + par);
             Console.WriteLine(" los numros impar son: " + impar);
             Console.WriteLine(" el promediode todo es: " + promedio);
-      
-
-
-}using System;
-
-class Program
-{
-    static void Main()
-    {
-        int cantidad;
-        int numero;
-        int positivos = 0;
-        int negativos = 0;
-        int ceros = 0;
-        int sumaPosi = 0;
-        int sumaNega = 0;
-
-        Console.Write("ponga la cantidad de numeros: ");
-        cantidad = int.Parse(Console.ReadLine());
-
-        for (int i = 1; i <= cantidad; i++)
-        {
-            Console.Write("ponga un numero: ");
-            numero = int.Parse(Console.ReadLine());
-
-            if (numero > 0)
-            {
-                positivos++;
-                sumaPosi = sumaPosi + numero;
-            }
-            else if (numero < 0)
-            {
-                negativos++;
-                sumaNega = sumaNega + numero;
-            }
-            else
-            {
-                ceros++;
-            }
-        }
-
-        MostrarResultados(positivos, negativos, ceros, sumaPosi, sumaNega);
-    }
-
-    static void MostrarResultados(int positivos, int negativos, int ceros, int sumaPosi, int sumaNega)
-    {
-        Console.WriteLine("positivos: " + positivos);
-        Console.WriteLine("negativos: " + negativos);
-        Console.WriteLine("ceros: " + ceros);
-
-        if (positivos > 0)
-        {
-            Console.WriteLine("el promedio de positivos son: " + (double)sumaPosi / positivos);
-        }
-
-        if (negativos > 0)
-        {
-            Console.WriteLine("el promedio de  negativos es: " + (double)sumaNega / negativos);
-        }
+    
     }
 }
     }
